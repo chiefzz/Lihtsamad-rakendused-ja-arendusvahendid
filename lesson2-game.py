f8b64e5a2a7bf84c265c694846778c01c508887c
chiefzz <enrichki234@gmail.com>
2022-03-18 22:51:40 +1400

    Second lesson - creating game

--- lesson2-game.py
+++ lesson2-game.py
@@ -1,1 +1,40 @@
-print("Hello")
+from hashlib import new
+
+
+map1 = [
+    [12, 0, 1, 0],
+    [1, 0, 1, 0],
+    [1, 0, 1, 0],
+    [1, 0, 0, 24]
+]
+map2 = [
+    [12, 0, 0, 0],
+    [1, 0, 1, 0],
+    [1, 0, 1, 0],
+    [0, 0, 0, 24]
+]
+map = map2
+
+# 1. First move
+start_position_x = 0
+start_position_y = 0
+
+def move(step, current_position_x, current_position_y):
+    print("Current step is: ", step)
+    print("Current current_position_y = ", current_position_y)
+    print("Current current_position_x = ", current_position_x)
+    can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 24
+    can_move_bottom = current_position_y <= 2 and map[current_position_y+1][current_position_x] == 24
+
+    if can_move_right:
+        print("Should move right")
+        current_position_x = current_position_x +1
+    if can_move_bottom:
+        print("Should move down")
+        current_position_y = current_position_y +1
+    return [current_position_x, current_position_y]
+    
+
+new_position = move(1, start_position_x, start_position_y)
+new_position = move(2, new_position[0], new_position[1])
+new_position = move(3, new_position[0], new_position[1])
